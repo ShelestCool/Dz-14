2b7e598a6675d7020089d3073e8d5ec4af552a88
ShelestCool <makar.shelest2@mail.ru>
2020-09-04 21:40:57 +0300

    v1

--- js/note.js
+++ js/note.js
@@ -1,12 +1,12 @@
-import { DnD } from './dnd';
-import imgSrc from '../img/btn.png';
+import { DnD } from "./dnd";
+import imgSrc from "../img/btn.png";
 
 export class Note {
   constructor(button) {
     this.data = [];
-    this.container = document.querySelector('.container'); // контейнер, нужен для изоляции заметок от остального html
+    this.container = document.querySelector(".container"); // контейнер, нужен для изоляции заметок от остального html
     this.button = button;
-    this.btnChacngeColor = document.querySelector('#changeColorNote');
+    this.btnChacngeColor = document.querySelector("#changeColorNote");
 
     this._handleClickButton = this._clickButton.bind(this);
     this.setCoords = this._setCoords.bind(this);
@@ -15,12 +15,12 @@ export class Note {
   }
 
   _init() {
-    this.button.addEventListener('click', this._handleClickButton);
+    this.button.addEventListener("click", this._handleClickButton);
   }
 
   // метод для записи координат в data, передаём его в класс DnD
   _setCoords(note, coords) {
-    const index = note.getAttribute('data-index');
+    const index = note.getAttribute("data-index");
 
     this.data[index].left = coords.x;
     this.data[index].top = coords.y;
@@ -38,7 +38,7 @@ export class Note {
 
   _clickButton() {
     const color = this.btnChacngeColor.value;
-    const newNoteObj = this._constructorNote('Hello', 48, 24, color); // передаём дефолтные значения
+    const newNoteObj = this._constructorNote("Hello", 48, 24, color); // передаём дефолтные значения
     this.data.push(newNoteObj);
 
     this.render();
@@ -64,40 +64,41 @@ export class Note {
 
   _createNote(data, index) {
     const [divNode, buttonNode, textareaNode] = [
-      document.createElement('div'),
-      document.createElement('div'),
-      document.createElement('textarea'),
+      document.createElement("div"),
+      document.createElement("div"),
+      document.createElement("textarea"),
     ];
 
     const noteNode = divNode.cloneNode(true);
-    noteNode.setAttribute('data-index', index); // index нужен, чтобы найти объект в массиве data
-    noteNode.classList.add('note');
+    noteNode.setAttribute("data-index", index); // index нужен, чтобы найти объект в массиве data
+    noteNode.classList.add("note");
     noteNode.style.cssText = `position: absolute; top: ${data.top}px; left: ${data.left}px; background-color: ${data.color}`;
     new DnD(noteNode, this.setCoords);
-    noteNode.addEventListener('dblclick', () => {
+    noteNode.addEventListener("dblclick", () => {
       this._editNote(textareaNode, contentNode, index);
     });
 
     const topContentNode = divNode.cloneNode(true);
-    topContentNode.classList.add('note__topContent');
+    topContentNode.classList.add("note__topContent");
 
     const btnCloseNode = buttonNode.cloneNode(true);
-    btnCloseNode.classList.add('note__close');
-    btnCloseNode.innerHTML = '<i class="fas fa-times fa-2x id="closeNode""></i>';
-    btnCloseNode.addEventListener('click', () => {
+    btnCloseNode.classList.add("note__close");
+    btnCloseNode.innerHTML =
+      '<i class="fas fa-times fa-2x id="closeNode""></i>';
+    btnCloseNode.addEventListener("click", () => {
       this._clickCloseNote(index);
     });
 
     const contentNode = divNode.cloneNode(true);
-    contentNode.classList.add('note__content');
+    contentNode.classList.add("note__content");
     contentNode.innerHTML = data.content;
 
-    textareaNode.classList.add('note__textarea');
+    textareaNode.classList.add("note__textarea");
     textareaNode.hidden = true;
     textareaNode.value = data.content;
 
-    const imgTopNote = document.createElement('img');
-    imgTopNote.classList.add('note__img');
+    const imgTopNote = document.createElement("img");
+    imgTopNote.classList.add("note__img");
     imgTopNote.src = imgSrc;
 
     noteNode.append(topContentNode, contentNode, textareaNode);
@@ -107,7 +108,7 @@ export class Note {
   }
 
   render() {
-    this.container.innerHTML = '';
+    this.container.innerHTML = "";
 
     this.data.forEach((noteObj, index) => {
       const noteNode = this._createNote(noteObj, index);
